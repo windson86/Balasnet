34581170de8a2b3c6c78c90dd39b3bd64030e935
ivekw <petrovic1986@gmail.com>
2021-10-14 23:04:14 +0200

    herokuworkaround

--- config/express.js
+++ config/express.js
@@ -1,12 +1,17 @@
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require("cors");
 
 module.exports = (app) => {
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(bodyParser.json())
-  app.use(passport.initialize())
-  app.use(cors())
-  console.log('Express ready!')
-}
-
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+  }
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(passport.initialize());
+  app.use(cors());
+  console.log("Express ready!");
+};
