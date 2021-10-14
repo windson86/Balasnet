5de04bfd49f9d469b4c022bd75143af1ff01ff86
ivekw <petrovic1986@gmail.com>
2021-10-14 22:36:25 +0200

    herokuworkaround

--- client/src/App.js
+++ client/src/App.js
@@ -1,108 +1,92 @@
-import React, { Component } from 'react'
-import Navbar from './components/common/Navbar'
-import Footer from './components/common/Footer'
+import React, { Component } from "react";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
 
-import Register from './components/Register'
-import Login from './components/Login'
-import Home from './components/Home'
-import Auth from './utils/auth'
-import Predvorje from './components/Predvorje'
+import Register from "./components/Register";
+import Login from "./components/Login/Login";
+import Home from "./components/Home";
+import Auth from "./utils/auth";
+import Predvorje from "./components/Predvorje";
 
 //import toastr from 'toastr'
-import { Switch, Route, withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { logoutAction } from './actions/auth-actions'
-import {getUserLevel} from './api/remote'
-
+import { Switch, Route, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutAction } from "./actions/auth-actions";
+import { getUserLevel } from "./api/remote";
 
 class App extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       loggedIn: false,
-      level:5
-    }
+      level: 5,
+    };
 
-    this.logout = this.logout.bind(this)
+    this.logout = this.logout.bind(this);
   }
 
- 
-  
-
-  componentDidMount () {
+  componentDidMount() {
     if (Auth.isUserAuthenticated()) {
-      
-        getUserLevel().then(data=>{
-          this.setState({ 
-            level: data.level,
-           
-           })
-        })
-     
-      this.setState({ 
+      getUserLevel().then((data) => {
+        this.setState({
+          level: data.level,
+        });
+      });
+
+      this.setState({
         loggedIn: true,
-       
-       })
-    }
-
-    
-  }
-
-  componentDidUpdate (nextProps) {
-    if (nextProps.loginSuccess !==this.props.loginSuccess) {
-      this.setState({ loggedIn: true })
+      });
     }
   }
 
-  logout () {
-    this.setState({ loggedIn: false })
-    this.props.logout()
+  componentDidUpdate(nextProps) {
+    if (nextProps.loginSuccess !== this.props.loginSuccess) {
+      this.setState({ loggedIn: true });
+    }
+  }
+
+  logout() {
+    this.setState({ loggedIn: false });
+    this.props.logout();
     //toastr.success('Logout successful')
-    this.props.history.push('/login')
+    this.props.history.push("/login");
   }
 
-  render () {
-     
-   const {level}=this.state
+  render() {
+    const { level } = this.state;
     return (
-      <div className='App'>
-       
-     <Navbar
-     username={Auth.getUsername()}
-     level={level}
-     logout={this.logout}
-     loggedIn={this.state.loggedIn}
-    
-     />
-             <main>
+      <div className="App">
+        <Navbar
+          username={Auth.getUsername()}
+          level={level}
+          logout={this.logout}
+          loggedIn={this.state.loggedIn}
+        />
+        <main>
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/predvorje' component={Predvorje} />
-            <Route path='/register' component={Register} />
-            <Route path='/login' component={Login} />
-            
+            <Route exact path="/" component={Home} />
+            <Route path="/predvorje" component={Predvorje} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
           </Switch>
-         
         </main>
-        <Footer/>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     loginSuccess: state.login.success,
-    
-  }
+  };
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     logout: () => dispatch(logoutAction()),
-   
-  }
+  };
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,108 +1,108 @@
-import React, {Component} from 'react'
-import Input from './common/Input'
-import loginValidator from '../utils/loginValidator'
+import React, { Component } from "react";
+import Input from "../common/Input";
+import loginValidator from "../../utils/loginValidator";
 //import toastr from 'toastr'
-import Auth from '../utils/auth'
-import {loginValidationFunc} from '../utils/formValidator'
-import {loginAction, redirectAction} from '../actions/auth-actions'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import Auth from "../../utils/auth";
+import { loginValidationFunc } from "../../utils/formValidator";
+import { loginAction, redirectAction } from "../../actions/auth-actions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 class LoginPage extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: "",
+    };
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     if (Auth.isUserAuthenticated()) {
-      this.props.history.push('/')
+      this.props.history.push("/");
     }
   }
 
-  componentDidUpdate (nextProps) {
+  componentDidUpdate(nextProps) {
     if (nextProps.loginError.hasError) {
-     // toastr.error(nextProps.loginError.message)
+      // toastr.error(nextProps.loginError.message)
     } else if (nextProps.loginSuccess) {
-        this.props.history.push('/')
-      this.props.redirect()
+      this.props.history.push("/");
+      this.props.redirect();
       //toastr.success('Login successful')
-     
     }
   }
 
-  onChange (e) {
-    this.setState({[e.target.name]: e.target.value})
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  onSubmit (e) {
-    e.preventDefault()
-    if (!loginValidator(this.state.email, this.state.password)) return
-    this.props.login(this.state.email, this.state.password)
-    this.props.history.push('/')
-    this.props.redirect()
+  onSubmit(e) {
+    e.preventDefault();
+    if (!loginValidator(this.state.email, this.state.password)) return;
+    this.props.login(this.state.email, this.state.password);
+    this.props.history.push("/");
+    this.props.redirect();
   }
 
-  render () {
-    let validObj = loginValidationFunc(
-      this.state.email,
-      this.state.password
-    )
+  render() {
+    let validObj = loginValidationFunc(this.state.email, this.state.password);
 
     return (
-      <div className='container'>
-        <div className='row space-top'>
-          <div className='col-md-12'>
+      <div className="container">
+        <div className="row space-top">
+          <div className="col-md-12">
             <h1>Login</h1>
           </div>
         </div>
         <form onSubmit={this.onSubmit}>
-          <div className='row space-top'>
-            <div className='col-md-4'>
+          <div className="row space-top">
+            <div className="col-md-4">
               <Input
-                type='text'
-                name='email'
-                label='E-mail'
-                placeholder='Enter e-mail'
+                type="text"
+                name="email"
+                label="E-mail"
+                placeholder="Enter e-mail"
                 value={this.state.email}
                 onChange={this.onChange}
-                valid={validObj.validEmail} />
+                valid={validObj.validEmail}
+              />
               <Input
-                type='password'
-                name='password'
-                label='Password'
-                placeholder='Enter password'
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="Enter password"
                 value={this.state.password}
                 onChange={this.onChange}
-                valid={validObj.validPassword} />
-              <input type='submit' className='btn btn-primary' value='Login' />
+                valid={validObj.validPassword}
+              />
+              <input type="submit" className="btn btn-primary" value="Login" />
             </div>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     loginSuccess: state.login.success,
-    loginError: state.loginError
-  }
+    loginError: state.loginError,
+  };
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     login: (email, password) => dispatch(loginAction(email, password)),
-    redirect: () => dispatch(redirectAction())
-  }
+    redirect: () => dispatch(redirectAction()),
+  };
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LoginPage))
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(LoginPage)
+);
