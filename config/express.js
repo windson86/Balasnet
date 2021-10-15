57e8bbb5705dc4817b8f48759f832cb4ac2d3efe
ivekw <petrovic1986@gmail.com>
2021-10-15 10:54:47 +0200

    heroku

--- config/express.js
+++ config/express.js
@@ -1,14 +1,16 @@
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
+const express = require("express");
 
 module.exports = (app) => {
-  if (process.env.NODE_ENV === "production") {
+  /* if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname + "/client/build/index.html"));
     });
-  }
+  } */
+  app.use(express.static("client/build"));
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(passport.initialize());
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const express = require("express");
 let env = process.env.NODE_ENV || "development";
 
 let settings = require("./config/settings")[env];
@@ -9,13 +8,6 @@ require("./config/database")(settings);
 require("./config/express")(app);
 require("./config/routes")(app);
 require("./config/passport")();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
 
 app.listen(settings.port);
 console.log(`Server listening on port ${settings.port}...`);
